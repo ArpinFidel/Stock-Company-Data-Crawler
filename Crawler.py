9dec689e830f714a409292a09a1b1de8718d9828
ArpinFidel <richardputra2910@gmail.com>
2018-12-28 04:48:09 +0700

    removed debug messages. added error messages. now prints all data

--- Crawler.py
+++ Crawler.py
@@ -19,14 +19,29 @@ runPath = os.path.dirname(os.path.abspath(__file__))
 
 op = webdriver.ChromeOptions()
 prefs = {	'profile.default_content_setting_values': {
-				'cookies': 2, 'images': 2,
-				'plugins': 2, 'popups': 2, 'geolocation': 2, 
-				'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2, 
-				'mouselock': 2, 'mixed_script': 2, 'media_stream': 2, 
-				'media_stream_mic': 2, 'media_stream_camera': 2, 'protocol_handlers': 2, 
-				'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2, 
-				'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2, 
-				'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 
+				'cookies': 2,
+				'images': 2,
+				'plugins': 2,
+				'popups': 2,
+				'geolocation': 2, 
+				'notifications': 2,
+				'auto_select_certificate': 2,
+				'fullscreen': 2, 
+				'mouselock': 2,
+				'mixed_script': 2,
+				'media_stream': 2, 
+				'media_stream_mic': 2,
+				'media_stream_camera': 2,
+				'protocol_handlers': 2, 
+				'ppapi_broker': 2,
+				'automatic_downloads': 2,
+				'midi_sysex': 2, 
+				'push_messaging': 2,
+				'ssl_cert_decisions': 2,
+				'metro_switch_to_desktop': 2, 
+				'protected_media_identifier': 2,
+				'app_banner': 2,
+				'site_engagement': 2, 
 				'durable_storage': 2},
 			'disk-cache-size': 4096}
 
@@ -51,16 +66,19 @@ companies = list()
 
 time.sleep(5)
 
+print('Loading data...')
+
 for i in range(7):
 	while True:
 		while  True:
 			try:
 				table = browser.find_element(By.ID, 'stockTable').find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
 				rowNum = int(table[0].find_elements(By.TAG_NAME, 'td')[0].get_attribute('innerHTML'))
-				print('table html:', table[0].get_attribute('innerHTML'))
-				print('rownum:', rowNum)
+				# print('table html:', table[0].get_attribute('innerHTML'))
+				# print('rownum:', rowNum)
 			except Exception as e:
 				print(e)
+				print('Failed getting table. Retrying...')
 				continue
 			break
 			
@@ -69,30 +87,31 @@ for i in range(7):
 		else:
 			time.sleep(0.5)
 	
-	print('table length:', len(table))
-	
 	for row in table:
 		while True:
 			try:
 				# print('row html:', row.get_attribute('innerHTML'))
 				data = row.find_elements(By.TAG_NAME, 'td')
-				print('data: ', data[1].get_attribute('innerHTML'))
+				# print('data: ', data[1].get_attribute('innerHTML'))
 			except Exception as e:
 				print(e)
-				print('failed. retrying')
+				print('Failed getting row. Retrying...')
 				continue
 			break
 		companies.append(Company(data))
+	
+	print('\nLoaded %d data\n' % (len(companies)))
 	
 	while True:
 		try:
 			buttonNext.click()
 		except Exception as e:
 			print(e)
+			print('Failled moving to next page. Retrying...')
 			buttonNext = browser.find_element(By.ID, 'stockTable_next')
 			time.sleep(0.5)
 			continue
 		break
 	
 for c in companies:
-	print(c.code)
+	print('%s^%s^%s^%s^%s' % (c.code, c.name, c.recDate, c.stock, c.board))
